7abc51f94afa3e8c65aad037c8fe82411ae3e6b8
Louis Ellis <louisellis@wr-130-64-98-169.medford.tufts.edu>
2012-11-30 18:23:47 -0500

    JSLint example working

--- xpi/chrome/content/noscript/nolint.js
+++ xpi/chrome/content/noscript/nolint.js
@@ -1,10 +1,77 @@
+
+
 function hello(){
+
 	
-	
-	alert("Cool Beans");
-	var scripts = document.getElementsByTagName("script");
- source = scripts[0].src;
-alert(scripts[0].src)
-console.log(scripts);
-	
-}
+    alert("Greate")
+    
+  // Run JSLINT over code
+            JSLINT("alert('hels')");
+            
+            var results = JSLINT.data();
+           
+           	alert(results.errors[0].reason);            
+           
+           
+           }
+           
+           // var myData = JSLINT.data();
+
+// It returns a structure with this form:
+
+// {
+// errors: [
+// {
+// line: NUMBER,
+// character: NUMBER,
+// reason: STRING,
+// evidence: STRING
+// }
+// ],
+// functions: [
+// {
+// name: STRING,
+// line: NUMBER,
+// last: NUMBER,
+// params: [
+// {
+// string: STRING
+// }
+// ],
+// closure: [
+// STRING
+// ],
+// var: [
+// STRING
+// ],
+// exception: [
+// STRING
+// ],
+// outer: [
+// STRING
+// ],
+// unused: [
+// STRING
+// ],
+// undef: [
+// STRING
+// ],
+// global: [
+// STRING
+// ],
+// label: [
+// STRING
+// ]
+// }
+// ],
+// globals: [
+// STRING
+// ],
+// member: {
+// STRING: NUMBER
+// },
+// urls: [
+// STRING
+// ],
+// json: BOOLEAN
+// }
