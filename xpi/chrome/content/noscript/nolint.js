c15bde0332e866b649a5e79c0b937afed143df1c
Louis Ellis <louisellis@wr-130-64-98-169.medford.tufts.edu>
2012-11-30 23:20:17 -0500

    JSLint integrated

--- xpi/chrome/content/noscript/nolint.js
+++ xpi/chrome/content/noscript/nolint.js
@@ -1,9 +1,59 @@
+function createDocument(html, title) {
+  var doc = document.implementation.createHTMLDocument(title)
+  doc.documentElement.innerHTML = html
+  return doc
+}
+
+
+
 function init(){
+	var currentWindow = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow("navigator:browser");
+
+    var currBrowser = currentWindow.getBrowser();
+    var currURL = currBrowser.currentURI.spec;
 	
+	var scripts = document.getElementsByTagName( 'script' );
+var thisScriptTag = scripts[ scripts.length - 1 ];
+
+
+	if("XMLHttpRequest" in window)
+		xmlhttp=new XMLHttpRequest();
+	if("ActiveXObject" in window)
+		xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+
+	var source;
+		
+	xmlhttp.open('GET',currURL,true);
+	xmlhttp.onreadystatechange=function() {
+	if (xmlhttp.readyState==4) {
+		source = xmlhttp.responseText;
+		
+		var doc = createDocument(source, 'test')
+		
+							
+		var scripts = doc.getElementsByTagName('script');
+		
+		var div1 = document.getElementById('codeBox');
+		div1.setAttribute('value','alert("sdfsdf")');
+
+
+		$.get(scripts[5].src, function(data){
+
+		
+		
+		if(data == null || data == undefined )
+			div1.setAttribute('value',data);
+
+				});
+						
+			}
+	};
 	
-	alert("Get all js files")
-	
-	
+	xmlhttp.send(null);void(0); 	
+
+
+ 
+
 }
 
 
@@ -20,20 +70,17 @@ function getfile(el){
 function hello(){
 
 	
-    alert("Greate")
     
-    var div1 = document.getElementById('codeBox');
-      div1.setAttribute('value','alert("sfsdf")');
+    		var div1 = document.getElementById('codeBox');
 
     var code = div1.getAttribute('value');
   
-    		alert(code)
             JSLINT(code);
             
             var results = JSLINT.data();
            
            
-           var elem=document.getElementById('resultBox');
+  var elem=document.getElementById('resultBox');
   elem.setAttribute('value',results.errors[0].reason);
 
            
